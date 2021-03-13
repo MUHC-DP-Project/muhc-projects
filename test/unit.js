64f1d1d265b407d20c02713f7e8706ec14d58aef
Max Brodeur-Urbas <murbas98@gmail.com>
2021-03-13 15:44:23 -0500

    Create project test :sparkles:

--- test/unit.js
+++ test/unit.js
@@ -1,8 +1,13 @@
-const assert = require('chai').assert;
+const { assert, expect } = require("chai");
+const chai = require("chai");
 const mongoose = require('mongoose');
 const connection = require('mongoose').connection;
 const { exit } = require('process');
 require('dotenv').config();
+const {sample_project1} = require("./sample_project");
+let baseUrl = "http://localhost:8080/projects/";
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 
 before(async() => {
     const MONGO_DB_URI = process.env.MONGO_TEST_DB_URI;
@@ -33,4 +38,19 @@ describe('App',function(){
     it('first test', function(){
         assert.equal(true, true);
     });
+});
+
+describe("create a project", () => {
+    it("return the project JSON", async () => {
+        return chai.request(baseUrl)
+            .post("")
+            .send(sample_project1)
+            .then((res) => {
+                expect(res).to.have.status(200);
+                assert.exists(res.body._id);
+            }).catch(err =>{
+                console.log(JSON.stringify(err));
+                assert.fail();
+        });
+    })
 });
