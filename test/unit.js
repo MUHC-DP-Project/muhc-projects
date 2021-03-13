8994e72ad2235602f5d817f0495c9cdeb8c00b43
preyansh98 <preyansh98@gmail.com>
2021-03-13 14:31:59 -0500

    :rocket: :rocket: :rocket: :rocket: :rocket: :fire:

--- test/unit.js
+++ test/unit.js
@@ -1,26 +1,36 @@
 const assert = require('chai').assert;
 const mongoose = require('mongoose');
+const connection = require('mongoose').connection;
 const { exit } = require('process');
 require('dotenv').config();
+
+before(async() => {
+    const MONGO_DB_URI = process.env.MONGO_TEST_DB_URI;
+    const options = {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    };
+    const resp = await mongoose.connect(MONGO_DB_URI, options); 
+});
+
+after(async() => {
+    mongoose.disconnect();
+    mongoose.connection.close();
+});
+
+afterEach(async() => {
+    const collections = Object.keys(mongoose.connection.collections);
+
+    for (const collectionName of collections) {
+        const collection = mongoose.connection.collections[collectionName];
+        await collection.deleteMany({});
+    }
+})
 
 describe('App',function(){
     it('first test', function(){
         assert.equal(true, true);
     });
 });
-
-describe('Database Connection', function() {
-    it('should check whether the database can be connected to', async function() {
-        const MONGO_DB_URI = process.env.MONGO_TEST_DB_URI;
-        const options = {
-          useNewUrlParser: true,
-          useCreateIndex: true,
-          useFindAndModify: false,
-          useUnifiedTopology: true,
-        };
-        const resp = await mongoose.connect(MONGO_DB_URI, options);
-        
-        //  kill db connection somehow
-        setTimeout(() => exit(), 2000);
-    })
-})
