7b2d008304ec5ef95b59c6fb9b672963e77c5b22
preyansh98 <preyansh98@gmail.com>
2021-03-28 19:52:08 -0400

    make changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 # ensure you heroku login and heroku container:login before
 
-docker build -t pbrn-users:v0.0.1 .
-docker tag pbrn-users:v0.0.1 registry.heroku.com/pbrn-users/web
-docker push registry.heroku.com/pbrn-users/web                  
-heroku container:release web -a pbrn-users
+docker build -t pbrn-projects:v0.0.1 .
+docker tag pbrn-projects:v0.0.1 registry.heroku.com/pbrn-projects/web
+docker push registry.heroku.com/pbrn-projects/web                  
+heroku container:release web -a pbrn-projects
