dfe48a7bbae26c4b041c6deb939e1cb258ccac1b
Preyansh Kaushik <preyansh98@gmail.com>
2021-04-05 20:33:07 -0400

    deploy (#34)
    * changed validators,schema and interface for the new form
    
    * Add authentication middleware
    
    * Update auth.middleware.ts
    
    * fix error with response headers
    
    Co-authored-by: ahmedazz260 <ahmed.azzouz@mail.mcgill.ca>
    Co-authored-by: Max Brodeur-Urbas <35381493+MaxBrodeurUrbas@users.noreply.github.com>

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import authMiddleware from "../middleware/auth.middleware";
 import { projectController } from "../controllers/project";
 import { projectValidator } from "../validators/project";
 
@@ -6,30 +7,35 @@ const projectRouter: Router = Router();
 
 projectRouter.get(
     "/",
+    authMiddleware,
     projectValidator("GET /projects"),
     projectController.getall
 );
 
 projectRouter.get(
     "/:projectId",
+    authMiddleware,
     projectValidator("GET /projects/:projectId"),
     projectController.show
 );
 
 projectRouter.post(
     "/",
+    authMiddleware,
     projectValidator("POST /projects"),
     projectController.create
 );
 
 projectRouter.put(
     "/:projectId",
+    authMiddleware,
     projectValidator("PUT /projects/:projectId"),
     projectController.update
 );
 
 projectRouter.delete(
     "/:projectId",
+    authMiddleware,
     projectValidator("DELETE /projects/:projectId"),
     projectController.delete
 );
